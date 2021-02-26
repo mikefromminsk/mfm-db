96d966e500182ec9f6829049bc7e83e78e30b79f
x29a100 <x29a100@mail.ru>
2021-02-26 13:29:43 +0300

    db/test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once $_SERVER["DOCUMENT_ROOT"] . "/db/db.php";
+
+$scalar = get("scalar");
+if ($scalar != null)
+    die(json_encode(scalar($scalar)));
+
+function assertEquals($message, $val, $need)
+{
+    if ($val != $need)
+        die("error $message val=$val need=$need");
+    echo "good $message\n";
+}
+
+
