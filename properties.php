771d43a8956d4aed94848971945d5f08097ae499
mike.haiduk <mike.haiduk@latoken.com>
2024-08-25 17:43:21 +0300

    add sockets

--- properties.php
+++ properties.php
@@ -1,9 +1,7 @@
 <?php
 
 $host_name = "localhost";
-$db_name = "darknode";
+$db_name = "tokens";
 $db_user = "root";
 $db_pass = "root";
 const DEBUG = true;
-
-$bscscan_api = "599AYTGK469V5FVTNI9RU8CQ46K794HMC8";
